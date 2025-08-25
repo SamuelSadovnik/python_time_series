2df88b58b33b1ad4cfadc1d508c7ada070d52596
SamuelSadovnik <samucvel@gmail.com>
2025-08-25 20:25:48 -0300

    exerc

---
+++ cadeira_preto.py
@@ -0,0 +1,5 @@
+# O cabeçalho de um arquivo Python é uma seção no início do arquivo que geralmente contém informações sobre o arquivo, como o nome do autor, a data de criação, uma descrição do que o arquivo faz e outras informações relevantes. Ele serve para documentar o código e fornecer contexto para quem está lendo o arquivo.
+
+# A principal diferença entre um cabeçalho e uma docstring é que o cabeçalho é geralmente colocado no início do arquivo, enquanto a docstring é usada para documentar funções, classes e módulos específicos dentro do código. A docstring fornece uma descrição mais detalhada do que uma função ou classe faz, incluindo informações sobre os parâmetros de entrada, o valor de retorno e possíveis exceções.
+
+# Uma docstring é uma string de documentação que é usada para descrever o propósito e o comportamento de uma função, classe ou módulo. Ela é escrita entre três aspas duplas e pode ser acessada programaticamente através do atributo `__doc__`. As docstrings são uma parte importante da documentação do código Python e ajudam os desenvolvedores a entender o que cada parte do código faz.
